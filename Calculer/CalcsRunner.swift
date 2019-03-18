19fd3d77d32787dec7b515ce55494853823468d1
Konrad Kołakowski <vashpan@gmail.com>
2019-03-18 23:52:11 +0100

    Added code to move new calculator instances a bit every spawn of a new one

--- Calculer/CalcsRunner.swift
+++ Calculer/CalcsRunner.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreFoundation
 import Cocoa
 
 class CalcsRunner {
@@ -16,19 +17,68 @@ class CalcsRunner {
     private var runningCalculators: [NSRunningApplication] = []
     
     private let calculatorAppURL: URL
+    private static let calculatorAppBundleId = "com.apple.calculator"
+    
+    private var realHomeDirectoryForCurrentUser: URL {
+        let pw = getpwuid(getuid())
+        let home = pw?.pointee.pw_dir
+        let homePath = FileManager.default.string(withFileSystemRepresentation: home!, length: Int(strlen(home)))
+        
+        return URL(fileURLWithPath: homePath, isDirectory: true)
+    }
     
     // MARK: Initialization
     private init() {
-        guard let calculatorAppPath = NSWorkspace.shared.absolutePathForApplication(withBundleIdentifier: "com.apple.calculator") else {
+        guard let calculatorAppPath = NSWorkspace.shared.absolutePathForApplication(withBundleIdentifier: CalcsRunner.calculatorAppBundleId) else {
             fatalError("Cannot find Calculator app in the system?!")
         }
 
         self.calculatorAppURL = URL(fileURLWithPath: calculatorAppPath)
     }
     
+    // MARK: Helpers
+    private func moveRecentCalculatorWindowABit() {
+        let calculatorPlistFilePath = self.realHomeDirectoryForCurrentUser.appendingPathComponent("Library/Containers/\(CalcsRunner.calculatorAppBundleId)/Data/Library/Preferences/\(CalcsRunner.calculatorAppBundleId).plist").path as CFString
+        let windowPositionPropertyKey = "NSWindow Frame Calc_Main_Window" as CFString
+        
+        // get initial value
+        guard let oldValue = CFPreferencesCopyAppValue(windowPositionPropertyKey, calculatorPlistFilePath) else {
+            NSLog("Can't get last calculator window position!")
+            return
+        }
+        let oldValueCFString = oldValue as! CFString
+        let oldValueString = oldValueCFString as String
+        
+        
+        // parse old position
+        let oldValueSplitted = oldValueString.split(separator: " ")
+        var windowPositions = oldValueSplitted.compactMap {
+            return Int($0)
+        }
+        
+        // move a bit down from initial position
+        if windowPositions.count >= 2 {
+            windowPositions[0] += 50
+            windowPositions[1] -= 50
+        }
+        
+        // convert back to string
+        let newValueSplitted = windowPositions.map {
+            return "\($0)"
+        }
+        let newValueString = newValueSplitted.joined(separator: " ")
+        let newValueCFString = newValueString as CFString
+        
+        // save new value
+        CFPreferencesSetAppValue(windowPositionPropertyKey, newValueCFString, calculatorPlistFilePath)
+        CFPreferencesAppSynchronize(calculatorPlistFilePath)
+    }
+    
     // MARK: Manage calculators
     func startNewCalculator() {
         do {
+            self.moveRecentCalculatorWindowABit()
+            
             let newCalculatorApp = try NSWorkspace.shared.launchApplication(at: self.calculatorAppURL,
                                                                             options: [.newInstance, .withoutAddingToRecents],
                                                                             configuration: [:])
